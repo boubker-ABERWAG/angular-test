90ba175d860a9f272ef734fbd76932c69c087dc5
Boubker ABERWAG <b_aberwag@yahoo.fr>
2019-03-20 14:16:04 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  s:string = 'Hello';
+
+  
+
+  tel:string = '0123456987';
+
+  vrai: boolean = true;
+
+  // enseignant
+  nom:string = "JACK";
+  prenom:String = "James";
+  numero:number = 125;
+  dateNaissance:string = '1990-01-01';
+  dateEmbauche:string = "2010-01-01";
+  sexe:string = 'M';
+  grade:string = 'ABC';
+  photo:string = 'http://www.aaege-israel.org/wp-content/uploads/2013/09/logo-enseignant.jpg'
+
+
+
+
 }
